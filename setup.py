e93837b100b910e2d317eb89c1564fe72c5457e4
Daniel Greenfeld <pydanny@gmail.com>
2009-05-31 20:03:30 -0400

    Adding skeleton project

---
+++ setup.py
@@ -0,0 +1,33 @@
+from setuptools import setup, find_packages
+import os
+
+version = '1.0'
+
+setup(name='Products.humanator',
+      version=version,
+      description="CAPTCHA alternative for Plone 3.x",
+      long_description=open("README.txt").read() + "\n" +
+                       open(os.path.join("docs", "HISTORY.txt")).read(),
+      # Get more strings from http://www.python.org/pypi?%3Aaction=list_classifiers
+      classifiers=[
+        "Framework :: Plone",
+        "Programming Language :: Python",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        ],
+      keywords='plone',
+      author='Daniel Greenfeld',
+      author_email='pydanny@gmail.com',
+      url='http://github.com/pydanny/Products.humanator',
+      license='BSD',
+      packages=find_packages(exclude=['ez_setup']),
+      namespace_packages=['Products'],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          'setuptools',
+          # -*- Extra requirements: -*-
+      ],
+      entry_points="""
+      # -*- Entry points: -*-
+      """,
+      )
